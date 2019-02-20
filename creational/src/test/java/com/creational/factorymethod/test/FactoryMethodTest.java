a9406aa7104651c3adfb06c9576010f3ea06ff39
Rey <reyvillaester@gmail.com>
2019-02-20 21:30:35 +0800

    abstract factory, singletone and prototype

--- creational/src/test/java/com/creational/factorymethod/test/FactoryMethodTest.java
+++ creational/src/test/java/com/creational/factorymethod/test/FactoryMethodTest.java
@@ -1,27 +1,30 @@
-package com.creational.factorymethod;
+package com.creational.factorymethod.test;
 
-import org.junit.gen5.api.Assertions;
-import org.junit.gen5.api.BeforeAll;
-import org.junit.gen5.api.Test;
+
+import com.creational.factorymethod.FriedChickenStore;
+import com.creational.factorymethod.FriedChickenStoreFactory;
+import com.creational.factorymethod.StoreType;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class FactoryMethodTest {
 
     @Test
     public void testMcdonaldsRate(){
         FriedChickenStore store = FriedChickenStoreFactory.getFriedChickenStore(StoreType.MCDONALDS);
-        Assertions.assertEquals(360.0, store.calculateBill(3));
+        Assert.assertEquals(360.0, store.calculateBill(3));
     }
 
     @Test
     public void testJollibeeRate(){
         FriedChickenStore store = FriedChickenStoreFactory.getFriedChickenStore(StoreType.JOLLIBEE);
-        Assertions.assertEquals(300.0, store.calculateBill(2));
+        Assert.assertEquals(300.0, store.calculateBill(2));
     }
 
     @Test
     public void testKennyRogersRate(){
         FriedChickenStore store = FriedChickenStoreFactory.getFriedChickenStore(StoreType.KENNYROGERS);
-        Assertions.assertEquals(500.0, store.calculateBill(5));
+        Assert.assertEquals(500.0, store.calculateBill(5));
     }
 
 
